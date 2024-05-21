c2dc7ac13e95f000f135ea6295c4b36c636252bc
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-05-21 10:04:10 +0900

    24-05-21

--- lab03_js/src/main/webapp/js/event.js
+++ lab03_js/src/main/webapp/js/event.js
@@ -19,7 +19,7 @@
  // TODO: input#itemInput2 요소에 'keydown' 이벤트 리스너를 등록:
  // 엔터키가 눌렸을 때, input에 입력된 내용을 ul#itemList2의 리스트 아이템으로 추가.
  const itemInput2 = document.querySelector('input#itemInput2');
- itemInput2.addEventListener('keydown', function (e) {
+ itemInput2.addEventListener('keydown', function (e) { // 'keydown'은 키보드를 입력할 때 이벤트 발생
     // const itemInput2 = document.querySelector('input#itemInput2');
     if (e.key === 'Enter') {
         const itemList2 = document.querySelector('ul#itemList2');
@@ -31,22 +31,23 @@
  // TODO: input#username 요소에 'change' 이벤트 리스너를 등록:
  // input에 입력된 내용이 바뀔 때마다 div를 입력 내용으로 덮어씀.
 const username = document.querySelector('input#username');
-username.addEventListener('change', function(e) {
-    const div = document.querySelector('div#output');
-    div.innerHTML = username.value;
+username.addEventListener('change', (e) => {
+    // 'change'는 input이 편집상태가 아니고(포커스를 잃어버린 상태), input에 입력된 값이 이전과 달라진 경우에 발생함.
+    const output = document.querySelector('div#output');
+    output.innerHTML = username.value;
     username.value = '';
 });
 
- // TODO: img#bulb 요소에 'mouseEnter' 이벤트 리스너를 등록:
+ // TODO: img#bulb 요소에 'mouseenter' 이벤트 리스너를 등록:
  // img의 src를 'images/bulb_on.gif'로 변경.
  const bulb_on = document.querySelector('img#bulb');
  bulb.addEventListener('mouseenter', function(e) {
     bulb_on.src = 'images/bulb_on.gif';
  });
  
- // TODO: img#bulb 요소에 'mouseLeave' 이벤트 리스너를 등록:
+ // TODO: img#bulb 요소에 'mouseleave' 이벤트 리스너를 등록:
  // img의 src를 'images/bulb_off.gif'로 변경.
 const bulb_off = document.querySelector('img#bulb');
- bulb.addEventListener('mouseleave', function(e) {
+ bulb.addEventListener('mouseleave', (e) => {
     bulb_off.src = 'images/bulb_off.gif';
  });

74b72798128723b7652dc01f987deff7e2fdc16a
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-05-21 17:18:37 +0900

    24-05-21

--- lab04_jsp/src/main/java/com/itwill/lab04/FirstServlet.java
+++ lab04_jsp/src/main/java/com/itwill/lab04/FirstServlet.java
@@ -17,17 +17,36 @@ public class FirstServlet extends HttpServlet {
      * @see HttpServlet#HttpServlet()
      */
     public FirstServlet() {
-    	
+    	System.out.println("FirstServlet 생성...");
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    // WAS(Web Application Server): 웹 요청(request) / 응답(response)을 처리하는 프로그램. Tomcat.
+    // doGet(): get 방식의 요청이 왔을 때 WAS가 호출하는 메서드.
+    // doPost(): post 방식의 요청이 왔을 때 WAS가 호출하는 메서드.
+    // 파라미터 request: 클라이언트가 서버로 보낸 요청의 정보, 기능들을 갖는 객체.
+    // 파라미터 response: 서버가 클라이언트로 보낼 응답의 정보, 기능들을 갖는 객체.
     @Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
     	System.out.println("FirstServlet::doGet() 호출");
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+    	response.setContentType("text/html; charset=UTF-8");
+    	
+    	response.getWriter()
+		.append("<!doctype html>")
+		.append("<html>") // Served at: /lab04
+		.append("<head>")
+		.append("<meta charset='UTF-8' />")
+		.append("<title>Servlet</title>")
+		.append("</head>")
+		.append("<body>")
+		.append("<h1>첫번째 Servlet</h1>")
+		.append("<a href='/lab04'>인덱스 페이지</a>")
+		.append("</body>")
+		.append("</html>");
 	}
 
 	/**
@@ -36,6 +55,7 @@ public class FirstServlet extends HttpServlet {
     @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+    	System.out.println("FirstServlet::doPost() 호출");
 		doGet(request, response);
 	}
 

1f4e1dec40a1bb3d237e5234545c8e3329bc4603
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-05-24 17:00:42 +0900

    24-05-24

--- lab04_jsp/src/main/java/com/itwill/lab04/filter/FilterEx.java
+++ lab04_jsp/src/main/java/com/itwill/lab04/filter/FilterEx.java
@@ -11,6 +11,11 @@ import jakarta.servlet.ServletResponse;
 /**
  * Servlet Filter implementation class FilterEx
  */
+// 필터 요청 주소 매핑 설정:
+// (1) web.xml(deployment descriptor) 파일에서 <filter>, <filter-mapping> 태그에서 설정하거나
+// (2) @WebFilter 애너테이션으로 설정.
+// web.xml과 애너테이션을 중복으로 설정하면 안 됨.
+// 필터 체인이 있을 때, 필터들이 실행되는 순서는 web.xml에 설정된 순서대로 실행됨.
 public class FilterEx extends HttpFilter {
        
     /**
@@ -26,6 +31,7 @@ public class FilterEx extends HttpFilter {
 	 */
     @Override
 	public void destroy() { // 필터가 메모리에서 삭제될 때(서버가 종료될 때) 호출됨. (딱 한 번)
+    	// WAS가 종료될 때 생성된 필터 객체를 소멸시키기 위해서 호출하는 메서드.
 		System.out.println("FilterEx::destroy() 호출");
 	}
 
@@ -35,10 +41,12 @@ public class FilterEx extends HttpFilter {
     @Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		System.out.println("FilterEx chain.doFilter() 호출 전"); // 서블릿 메서드가 호출되기 전 해야 할일
+		// 클라이언트로부터 온 요청을 필터 체인(-> 서블릿)으로 전달하기 전에 실행할 코드를 작성.
 
-		// pass the request along the filter chain
+		// 요청을 필터 체인으로 전달 -> 요청 주소에 매핑된 서블릿 메서드 호출.
 		chain.doFilter(request, response); // 이게 없으면 서블릿 메서드를 호출하지 X
 		
+		// 요청 처리가 끝난 후 실행할 코드를 작성.
 		System.out.println("FilterEx chain.doFilter() 호출 후"); // 서블릿 메서드가 호출된 후 해야 할일
 	}
 
@@ -47,6 +55,7 @@ public class FilterEx extends HttpFilter {
 	 */
     @Override
 	public void init(FilterConfig fConfig) throws ServletException { // 서버가 시작될 때(초기화될 때) 호출됨. (딱 한 번)
+    	// 필터가 생성된 후 초기화 작업을 수행하기 위해서 호출되는 메서드.
 		System.out.println("FilterEx::init() 호출");
 	}
 

57bae35885c3b75d12006227d68fc7f2dcef6045
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-05-24 11:20:36 +0900

    24-05-24

--- lab04_jsp/src/main/java/com/itwill/lab04/controller/ContactController.java
+++ lab04_jsp/src/main/java/com/itwill/lab04/controller/ContactController.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import com.itwill.lab04.model.Contact;
+
 /**
  * Servlet implementation class ContactController
  */
@@ -32,6 +34,23 @@ public class ContactController extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("ContactController::doPost() 호출");
+		
+		// 클라이언트가 POST 방식으로 보낸 요청 파라미터 값을 읽음.
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String phone = request.getParameter("phone");
+		String email = request.getParameter("email");
+		System.out.printf("id=%d, name=%s, phone=%s, email=%s\n", id, name, phone, email);
+		
+		// 요청 파라미터 값들을 이용해서 서비스에 필요한 작업.
+		Contact c = new Contact(id, name, phone, email);
+		System.out.println(c);
+		
+		// 인덱스 페이지로 리다이렉트(redirect)
+		response.sendRedirect("/lab04");
+		
+		// PRG(Post 요청 - Redirect - Get 요청) 패턴.
+		
 	}
 
 }
--- lab04_jsp/src/main/webapp/WEB-INF/views/contact.jsp
+++ lab04_jsp/src/main/webapp/WEB-INF/views/contact.jsp
@@ -12,6 +12,25 @@
     
     <main>
         <h1>MVC(Model-View-Controller)</h1>
+        <h2>연락처 입력 페이지</h2>
+        <form action="mvc" method="post">
+            <div>
+                <input type="number" name="id" placeholder="번호 입력" required autofocus />
+            </div>
+            <div>
+                <input type="text" name="name" placeholder="이름" required />
+            </div>
+            <div>
+                <input type="text" name="phone" placeholder="전화번호" required />
+            </div>
+            <div>
+                <input type="email" name="email" placeholder="이메일" />
+            </div>
+            <div>
+                <input type="submit" value="저장" />
+                <input type="reset" value="취소" />
+            </div>
+        </form>
     </main>
 </body>
 </html>

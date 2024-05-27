803d7d309ae12f9c20a712f84cf65c6b7de06cfe
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-05-27 14:20:13 +0900

    24-05-27

--- lab05_jsp/src/main/java/com/itwill/lab05/web/FrontController.java
+++ lab05_jsp/src/main/java/com/itwill/lab05/web/FrontController.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Servlet implementation class FrontController
  */
@@ -14,6 +17,7 @@ import java.io.IOException;
 // context root(http://localhost:8080/lab05)로 들어오는 요청을 처리하는 서블릿.
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final Logger log = LoggerFactory.getLogger(FrontController.class);
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -21,6 +25,11 @@ public class FrontController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("FrontController#doGet");
+//		log.debug("doGet"); // 13:56:58.598 DEBUG [com.itwill.lab05.web.FrontController    ] doGet
+//		log.info("doGet"); // 14:12:42.516 INFO  [com.itwill.lab05.web.FrontController    ] doGet
+//		log.warn("doGet"); // 14:13:23.396 WARN  [com.itwill.lab05.web.FrontController    ] doGet
+//		log.error("doGet"); // 14:13:54.458 ERROR [com.itwill.lab05.web.FrontController    ] doGet
+		log.trace("doGet"); // FrontController#doGet (log4j2.xml에서 "Root level"을 "debug"로 했을 경우
 		
 		request.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
 	}

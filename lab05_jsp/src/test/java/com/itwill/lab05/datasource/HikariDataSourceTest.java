adafe8e2d514b5fc9a449c7cf2676fc08090e6c4
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-05-27 15:19:55 +0900

    24-05-27

---
+++ lab05_jsp/src/test/java/com/itwill/lab05/datasource/HikariDataSourceTest.java
@@ -0,0 +1,16 @@
+package com.itwill.lab05.datasource;
+
+import org.junit.jupiter.api.Test;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+public class HikariDataSourceTest {
+	
+	private final DataSourceUtil util = DataSourceUtil.getInstance();
+	
+	@Test
+	public void test() {
+		System.out.println("test...");
+	}
+
+}

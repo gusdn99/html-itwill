27ddd10a516e84ede44eddb484716ba5df7c8c79
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-06-13 00:23:07 +0900

    Update PostSearchDto.java

--- spring2/src/main/java/com/itwill/spring2/dto/PostSearchDto.java
+++ spring2/src/main/java/com/itwill/spring2/dto/PostSearchDto.java
@@ -1,46 +1,11 @@
 package com.itwill.spring2.dto;
 
-import java.time.LocalDateTime;
-
-import com.itwill.spring2.repository.Post;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+//검색 요청에서의 요청 파라미터 검색 카테고리와 검색어를 저장하기 위한 DTO
 public class PostSearchDto {
 	private String category;
 	private String keyword;
-	private Integer id;
-	private String title;
-	private String author;
-	private LocalDateTime modifiedTime;
-	
-	public static PostSearchDto fromEntity(Post post) {
-        return PostSearchDto.builder()
-				.id(post.getId())
-				.title(post.getTitle())
-				.author(post.getAuthor())
-				.modifiedTime(post.getModifiedTime())
-                .build();
-    }
-
-//	private String title;
-//    private String content;
-//    private String author;
-//    
-//    public static PostSearchDto fromEntity(Post post) {
-//        return PostSearchDto.builder()
-//                .title(post.getTitle())
-//                .content(post.getContent())
-//                .author(post.getAuthor())
-//                .build();
-//    }
 
 }
-

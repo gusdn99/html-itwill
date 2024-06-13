ef5cdf190bd7ae6b38dcbb36841e7991b94fc46e
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-06-13 09:29:48 +0900

    24-06-12

--- spring2/src/main/java/com/itwill/spring2/dto/PostSearchDto.java
+++ spring2/src/main/java/com/itwill/spring2/dto/PostSearchDto.java
@@ -7,22 +7,5 @@ import lombok.Data;
 public class PostSearchDto {
 	private String category;
 	private String keyword;
-<<<<<<< HEAD
-	private Integer id;
-	private String title;
-	private String author;
-	private LocalDateTime modifiedTime;
-	
-	public static PostSearchDto fromEntity(Post post) {
-        return PostSearchDto.builder()
-				.id(post.getId())
-				.title(post.getTitle())
-				.author(post.getAuthor())
-				.modifiedTime(post.getModifiedTime())
-                .build();
-    }
-
-=======
->>>>>>> branch 'main' of https://github.com/gusdn99/html-itwill.git
 
 }

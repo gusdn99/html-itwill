6d10cd8cada950273da86186b7f99ebd80983fb9
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-06-14 10:20:03 +0900

    24-06-14

--- spring2/src/main/java/com/itwill/spring2/dto/CommentUpdateDto.java
+++ spring2/src/main/java/com/itwill/spring2/dto/CommentUpdateDto.java
@@ -5,7 +5,6 @@ import com.itwill.spring2.repository.Comment;
 import lombok.Data;
 
 @Data
-// 업데이트 요청의 요청 파라미터들을 저장하기 위한 DTO
 public class CommentUpdateDto {
 	private int id;
 	private String ctext;
--- spring2/src/main/java/com/itwill/spring2/service/CommentService.java
+++ spring2/src/main/java/com/itwill/spring2/service/CommentService.java
@@ -1,10 +1,12 @@
 package com.itwill.spring2.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.itwill.spring2.dto.CommentCreateDto;
+import com.itwill.spring2.dto.CommentItemDto;
 import com.itwill.spring2.dto.CommentUpdateDto;
 import com.itwill.spring2.repository.Comment;
 import com.itwill.spring2.repository.CommentDao;
@@ -18,14 +20,25 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class CommentService {
 
-	private final CommentDao commentDao;
+	private final CommentDao commentDao; // 생성자에 의한 의존성 주입
 	
-	public List<Comment> read(Integer postId) {
-		log.debug("read({})", postId);
+	public List<CommentItemDto> readByPostId(Integer postId) {
+		log.debug("readByPostId(postId={})", postId);
+		
+		// 리포지토리(영속성) 계층의 메서드를 호출해서 comments 테이블의 데이터를 검색.
 		List<Comment> list = commentDao.selectByPostId(postId);
 		
-		return list;
+		// List<Comment>를 List<CommentItemDto>로 변환해서 리턴.
+//		List<CommentItemDto> result = new ArrayList<>();
+//		for (Comment c : list) {
+//			CommentItemDto dto = CommentItemDto.fromEntity(c);
+//			result.add(dto);
+//		}
+//		return result;
 		
+		return list.stream()
+              .map(CommentItemDto::fromEntity) // map((x) -> CommentItemDto.fromEntity(x))
+              .toList();
 	}
 	
 	public int create(CommentCreateDto dto) {
@@ -87,6 +100,14 @@ public class CommentService {
 //                .toList();
 //    }
 	
+//    public CommentListDto readById(Integer id) {
+//        log.debug("readById(id={})", id);
+//        
+//        Comment comment = commentDao.selectById(id);
+//        log.debug(comment.toString());
+//        
+//        return CommentListDto.fromEntity(comment);
+//    }
 
 }
 

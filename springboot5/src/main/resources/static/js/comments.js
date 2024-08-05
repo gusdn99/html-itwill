59d96d372f68b13cc0fe3d1e4d17b17bde044919
gusdn99 <167641195+gusdn99@users.noreply.github.com>
2024-08-05 15:00:57 +0900

    24-08-05

--- springboot5/src/main/resources/static/js/comments.js
+++ springboot5/src/main/resources/static/js/comments.js
@@ -88,7 +88,8 @@ document.addEventListener('DOMContentLoaded', () => {
             
             // 현재 페이지 번호보다 페이지 개수가 더 많으면 댓글 [더보기] 버튼을 보여줌.
             const divBtnMore  = document.querySelector('div#divBtnMore');
-            if (currentPageNo + 1 < response.data.totalPages) { // currentPageNo가 기본적으로 0이라 1을 더함.
+            // if (currentPageNo + 1 < response.data.totalPages) { // currentPageNo가 기본적으로 0이라 1을 더함.
+            if (!response.data.last) {
                 divBtnMore.classList.remove('d-none');
             } else {
                 divBtnMore.classList.add('d-none');
